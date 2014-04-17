66b3e3b8b5523e151487c6eacd6dadc95dceca0e
koori <koori@AFN-koori.local>
2014-04-17 17:42:08 +0900

    3ヶ月表示の完成。コンボボックスからは指定の月を表示することは可能

--- index.php
+++ index.php
@@ -4,17 +4,23 @@ $now = time();
 $today = getdate($now);
 $year = $today['year'];
 $month = $today['mon'];
+//月のスタートと終わり
 $start_day = 1;
-$end_day = date('t');
+//カレンダー数
+$display_count = 3;
+$calendars = array();
+$end_day = array();
 
-if (isset($_GET['ym'])) {
-    $year = date('Y',strtotime($_GET['ym'].'-1'));
-    $month = date('n',strtotime($_GET['ym'].'-1'));
-}
-// print_r($year);
+//GET値がある場合。ない場合は現在の年月
+$ym = isset($_GET['ym']) ? $_GET['ym']:($year.'-'.$month);
+$explode_ym = explode('-', $ym);//[0] => 2014 [1] => 5
+$year_of_ym = $explode_ym[0];
+$month_of_ym = $explode_ym[1];
+print_r($year_of_ym.'-'.$month_of_ym);
 
 //今月
-$this_month = strtotime($year.$month.'1');//1397646881
+$this_month = strtotime($year.'-'.$month.'-01');//1397646881
+$date_this_month = date('Y-n', $this_month);
 
 //先月
 $last_month = array(
@@ -28,72 +34,80 @@ $next_month = array(
   'month' => date('n', strtotime('next month', $this_month))
 );
 
-//先月
-// $last_month = date('m', strtotime('last month', $month));
-// print_r($last_month);
+//前後の空セル
+$before = date('w', mktime(0, 0, 0, $month_of_ym, 1, $year_of_ym)); //1日の曜日を数値で取得
+$after = date('w', mktime(0, 0, 0, $month_of_ym + 1, 0, $year_of_ym));
 
+// print_r(date('w', mktime(0, 0, 0, 1, 1, 2014)));
 
-/*
-$now_year  = date('Y'); //現在の年を取得
-$now_month = date('n'); //現在の月を取得
-$now_day   = date('j'); //現在の日を取得
-if(isset($_GET['ym']))
-{
-  $now_year  = date('Y',strtotime($_GET['ym'].'-1'));
-  $now_month = date('n',strtotime($_GET['ym'].'-1'));
+// date("w", strtotime($value.'-'.$day))  year_of_ym
+//$prev_month2 = $month -1;
+$prev_month = $month_of_ym -1;
+$prev_month2 = $month_of_ym -1;
+$prev_month3 = $month_of_ym -1;
+$prev_month4 = $month_of_ym -1;
+// print_r($prev_month);exit;
+$before_cell = array();
+$after_cell = array();
+//奇数月と偶数月
+if ($display_count % 2 == 1) {
+    for ($i=1; $i<=$display_count; $i++) {
+        $position = $i-(floor($display_count/2)+1);
+        $calendars[] = date("Y-n", mktime(0, 0, 0, $prev_month++, 1, $year_of_ym));
+        $before_cell[] = date('w', mktime(0, 0, 0, $prev_month2++, 1, $year_of_ym));
+        $after_cell[]  = date('w', mktime(0, 0, 0, $prev_month3+1, 0, $year_of_ym));
+        $prev_month3++;
+        $end_day[] = date('t', mktime(0,0,0, $prev_month4++, 1, $year_of_ym));
+    }
 }
+/*
+print_r($calendars[0].":".$before_cell[0]."<br />");
+print_r($calendars[1].":".$before_cell[1]."<br />");
+print_r($calendars[2].":".$before_cell[2]."<br />");
 */
 
-
-$next_next_month = $prev_month+2;
-
-//前月、翌月リンク
-$prev = date('Y-n', mktime(0, 0, 0, $month-1, 1, $year));
-$now  = date('Y-n', mktime(0, 0, 0, $month, 1, $year));
-$next = date('Y-n', mktime(0, 0, 0, $month+1, 1, $year));
+/*
+print_r($calendars[0].":".$after_cell[0]."<br />");
+print_r($calendars[1].":".$after_cell[1]."<br />");
+print_r($calendars[2].":".$after_cell[2]."<br />");
+*/
+/*
+print_r($calendars[0].":".$end_day[0]."<br />");
+print_r($calendars[1].":".$end_day[1]."<br />");
+print_r($calendars[2].":".$end_day[2]."<br />");
+exit;
+*/
+//GET値がある場合、月に-1または+1する
+/*
+if (isset($ym)) {
+    $last_month = array(
+        'year' => date('Y', strtotime('last month', $explode[0])),
+        'month' => $explode[1]-1
+    );
+    $next_month = array(
+        'year' => date('Y', strtotime('next month', $this_month)),
+        'month' => $explode[1]+1
+    );
+    // print_r($next_month['month']);
+}
+*/
+if (isset($ym)) {
+    $last_month = array(
+        'year' => date('Y', strtotime('last month', $explode[0])),
+        'month' => $explode[1]-1
+    );
+    $next_month = array(
+        'year' => date('Y', strtotime('next month', $this_month)),
+        'month' => $explode[1]+1
+    );
+    // print_r($next_month['month']);
+}
 
 // Y-nを取得。$now_yeaeの前後1年
 for ($i=-12; $i<=12; $i++) {
     $months[] = date('Y-n', mktime(0, 0, 0, $month+($i), 1, $year));
 }
 
-$prev_month_day = 1;
-$next_month_day = 1;
-
-
-
-$wd1      = date('w', mktime(0, 0, 0, $month, 1, $year)); //1日の曜日を数値で取得
-$wdx      = date('w', mktime(0, 0, 0, $month + 1, 0, $year));
-$prev_wd1 = date('w', mktime(0, 0, 0, $month, 1, $year)); //1日の曜日を数値で取得
-$prev_wdx = date('w', mktime(0, 0, 0, $month + 1, 0, $year));
-$next_wd1 = date('w', mktime(0, 0, 0, $month, 1, $year)); //1日の曜日を数値で取得
-$next_wdx = date('w', mktime(0, 0, 0, $month + 1, 0, $year));
-
-
-/*Google Calendar API*/
-/*
-$last_year = date('Y-01-01', mktime(0, 0, 0, $now_month, 1, $now_year-1)); //今から1年前
-$next_year = date('Y-12-31', mktime(0, 0, 0, $now_month, 1, $now_year+1)); //今から1年後
-
-$holidays_url = sprintf(
-    'http://74.125.235.142/calendar/feeds/%s/public/full-noattendees?start-min=%s&start-max=%s&max-results=%d&alt=json',
-    'outid3el0qkcrsuf89fltf7a4qbacgt9@import.calendar.google.com',
-    $last_year,    // 取得開始日
-    $next_year,    // 取得終了日
-    100                // 最大取得数
-);
-
-if($results=file_get_contents($holidays_url)) {
-    $results  = json_decode($results, true);
-    $holidays = array();
-    foreach($results['feed']['entry'] as $val) {
-        $date  = $val['gd$when'][0]['startTime']; // 日付を取得
-        $title = $val['title']['$t']; // 何の日かを取得
-        $holidays[$date] = $title; // 日付をキーに、祝日名を値に格納
-    }
-    ksort($holidays); // 日付順にソート
-}
-*/
 ?>
 
 <!DOCTYPE html>
@@ -108,9 +122,10 @@ if($results=file_get_contents($holidays_url)) {
 <h3>郡カレンダー</h3>
 <div id="prev"><a href="?ym=<?php echo $last_month['year'].'-'.$last_month['month'];?>">先月</a></div>
 <div id="this"><a href="/">今月</a></div>
-<div id="next"><a href="?ym=<?php echo $next_month['year'].'-'.$next_month['month'];?>">来月</a></div>
+<div id="next"><a href="?ym=<?php echo $next_month['year'].'-'.$next_month['month']; ?>">来月</a></div>
 <form method="get" action="<?php $_SERVER['PHP_SELF']; ?>">
     <select name="ym">
+    <option>選択してください</option>
     <?php for ($i=0; $i<=24; $i++):?>
     <option id="select_year_month" value="<?php echo $months[$i] ;?>"><?php echo $months[$i] ;?></option>
     <?php endfor; ?>
@@ -120,75 +135,60 @@ if($results=file_get_contents($holidays_url)) {
 </div><!--header-->
 
 <div id="calendar">
-<?php //for($months=$last_month['month']; $months<=$next_month['next_month']; $months++):?>
+<!-- カレンダーループ 3回ループ -->
+<?php foreach ($calendars as $key => $value) :?>
 <table class="main_calendar">
     <thead>
     <tr>
-       <th colspan="7">
-        <?php
-            if (!isset($_GET['ym'])) {
-                echo $now;
-            }
-            else {
-                echo $now;
-            }
-        ?>
-      </th>
+        <th colspan="7">
+            <?php echo $value;?>
+        </th>
     </tr>
     <tr>
-    <th class="sunday">日</th>
-    <th>月</th>
-    <th>火</th>
-    <th>水</th>
-    <th>木</th>
-    <th>金</th>
-    <th class="saturday">土</th>
-
+        <th class="sunday">日</th>
+        <th>月</th>
+        <th>火</th>
+        <th>水</th>
+        <th>木</th>
+        <th>金</th>
+        <th class="saturday">土</th>
     </tr>
     </thead>
     <tbody>
     <tr>
-    <!-- 空セル挿入 -->
-    <?php for($i=1; $i<=$wd1; $i++) :?>
-        <td></td>
-    <?php endfor ;?>
-
-        <?php //while (checkdate($month, $start_day, $year)) :?>
+        <!-- 空セル挿入 -->
+        <?php for($i=1; $i<=$before_cell[$key]; $i++) :?>
+            <td></td>
+        <?php endfor ;?>
 
         <!-- 日付挿入 -->
-        <?php for ($start_day=$start_day; $start_day<=$end_day; $start_day++):?>
-            <?php $month_weekend=date("w", mktime(0, 0, 0, $month, $start_day, $year));?>
+        <?php for ($day=$start_day; $day<=$end_day[$key]; $day++):?>
+            <!-- 週末取得0~6 -->
+            <?php $month_weekend=date("w", strtotime($value.'-'.$day));?>
             <!-- 週末 -->
             <?php if ($month_weekend == 0) :?>
-                <td class="sunday"><?php echo $start_day ;?></td>
+                <tr><td class="sunday"><?php echo $day ;?></td>
             <?php endif ;?>
             <?php if ($month_weekend == 6) :?>
-                <td class="saturday"><?php echo $start_day ;?></td></tr>
+                <td class="saturday"><?php echo $day ;?></td></tr>
             <?php endif ;?>
             <!-- 週末以外 -->
             <?php if ($month_weekend != 0 && $month_weekend != 6) :?>
-                <td><?php echo $start_day ;?></td>
+                <?php if (date('n') == $month && $day == $today['mday']) :?>
+                    <td class="today"><?php echo $day ;?></td>
+                <?php else :?>
+                    <td><?php echo $day ;?></td>
+                <?php endif ;?>
             <?php endif ;?>
         <?php endfor ;?>
 
-        <?php //endwhile ;?>
-
-        <!-- 土曜日で改行する -->
-        <?php //if ($month_weekend == 6) :?>
-        <!-- </tr> -->
-        <?php //endif ;?>
-        <!-- 新たな週を準備 -->
-        <?php //if (checkdate($month, $start_day + 1, $year)) :?>
-            <!-- <tr> -->
-        <?php //endif ;?>
-
-      <!-- 空セル挿入 -->
-      <?php for ($i = 1; $i < 7 -$wdx; $i++) :?>
-          <td></td>
-      <?php endfor ;?>
+        <!-- 空セル挿入 -->
+        <?php for ($i=1; $i<(7-$after_cell[$key]); $i++) :?>
+            <td></td>
+        <?php endfor ;?>
     </tbody>
 </table>
-<?php //endfor ;?>
+<?php endforeach ;?>
 </div><!--calendar-->
 
 
