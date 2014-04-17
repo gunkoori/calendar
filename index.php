4140d0ba571fc6f280271804473bda70613a89b3
koori <koori@AFN-koori.local>
2014-04-17 18:06:29 +0900

    来月、先月の対応も含め3ヶ月表示の完成。あとは当日のセルの色付け修正。

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ $now = time();
 $today = getdate($now);
 $year = $today['year'];
 $month = $today['mon'];
-//月のスタートと終わり
+//月のスタート
 $start_day = 1;
 //カレンダー数
 $display_count = 3;
@@ -16,7 +16,6 @@ $ym = isset($_GET['ym']) ? $_GET['ym']:($year.'-'.$month);
 $explode_ym = explode('-', $ym);//[0] => 2014 [1] => 5
 $year_of_ym = $explode_ym[0];
 $month_of_ym = $explode_ym[1];
-print_r($year_of_ym.'-'.$month_of_ym);
 
 //今月
 $this_month = strtotime($year.'-'.$month.'-01');//1397646881
@@ -24,31 +23,26 @@ $date_this_month = date('Y-n', $this_month);
 
 //先月
 $last_month = array(
-  'year' => date('Y', strtotime('last month', $this_month)),
-  'month' => date('n', strtotime('last month', $this_month))
+  'year' => date('Y', strtotime('last month', strtotime($year_of_ym.'-'.$month_of_ym.'-01'))),
+  'month' => date('n', strtotime('last month', strtotime($year_of_ym.'-'.$month_of_ym.'-01')))
 );
-
 //来月
 $next_month = array(
-  'year' => date('Y', strtotime('next month', $this_month)),
-  'month' => date('n', strtotime('next month', $this_month))
+  'year' => date('Y', strtotime('next month', strtotime($year_of_ym.'-'.$month_of_ym.'-01'))),
+  'month' => date('n', strtotime('next month', strtotime($year_of_ym.'-'.$month_of_ym.'-01')))
 );
 
 //前後の空セル
 $before = date('w', mktime(0, 0, 0, $month_of_ym, 1, $year_of_ym)); //1日の曜日を数値で取得
-$after = date('w', mktime(0, 0, 0, $month_of_ym + 1, 0, $year_of_ym));
+$after  = date('w', mktime(0, 0, 0, $month_of_ym + 1, 0, $year_of_ym));
 
-// print_r(date('w', mktime(0, 0, 0, 1, 1, 2014)));
-
-// date("w", strtotime($value.'-'.$day))  year_of_ym
-//$prev_month2 = $month -1;
-$prev_month = $month_of_ym -1;
+$prev_month  = $month_of_ym -1;
 $prev_month2 = $month_of_ym -1;
 $prev_month3 = $month_of_ym -1;
 $prev_month4 = $month_of_ym -1;
-// print_r($prev_month);exit;
 $before_cell = array();
-$after_cell = array();
+$after_cell  = array();
+
 //奇数月と偶数月
 if ($display_count % 2 == 1) {
     for ($i=1; $i<=$display_count; $i++) {
@@ -59,48 +53,6 @@ if ($display_count % 2 == 1) {
         $prev_month3++;
         $end_day[] = date('t', mktime(0,0,0, $prev_month4++, 1, $year_of_ym));
     }
-}
-/*
-print_r($calendars[0].":".$before_cell[0]."<br />");
-print_r($calendars[1].":".$before_cell[1]."<br />");
-print_r($calendars[2].":".$before_cell[2]."<br />");
-*/
-
-/*
-print_r($calendars[0].":".$after_cell[0]."<br />");
-print_r($calendars[1].":".$after_cell[1]."<br />");
-print_r($calendars[2].":".$after_cell[2]."<br />");
-*/
-/*
-print_r($calendars[0].":".$end_day[0]."<br />");
-print_r($calendars[1].":".$end_day[1]."<br />");
-print_r($calendars[2].":".$end_day[2]."<br />");
-exit;
-*/
-//GET値がある場合、月に-1または+1する
-/*
-if (isset($ym)) {
-    $last_month = array(
-        'year' => date('Y', strtotime('last month', $explode[0])),
-        'month' => $explode[1]-1
-    );
-    $next_month = array(
-        'year' => date('Y', strtotime('next month', $this_month)),
-        'month' => $explode[1]+1
-    );
-    // print_r($next_month['month']);
-}
-*/
-if (isset($ym)) {
-    $last_month = array(
-        'year' => date('Y', strtotime('last month', $explode[0])),
-        'month' => $explode[1]-1
-    );
-    $next_month = array(
-        'year' => date('Y', strtotime('next month', $this_month)),
-        'month' => $explode[1]+1
-    );
-    // print_r($next_month['month']);
 }
 
 // Y-nを取得。$now_yeaeの前後1年
