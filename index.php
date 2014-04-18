53464a18875b2a7aaebaa10307159e6191400ec2
koori <koori@AFN-koori.local>
2014-04-18 15:35:13 +0900

    祝日の取得

--- index.php
+++ index.php
@@ -1,9 +1,15 @@
 <?php
+define(GOOGLE_CAL_URL, 'japanese__ja@holiday.calendar.google.com');
+//日付のタームゾーンを変更
+ini_set("date.timezone", "Asia/Tokyo");
+
 //現在の年月日、曜日の取得
-$now = time();
-$today = getdate($now);
-$year = $today['year'];
-$month = $today['mon'];
+// $now = time();
+// $today = getdate($now);
+// $year = $today['year'];
+$year = date('Y');
+$month = date('m');
+// print_r($month);
 //月のスタート
 $start_day = 1;
 //カレンダー数
@@ -13,19 +19,19 @@ $end_day = array();
 
 //GET値がある場合。ない場合は現在の年月
 $ym = isset($_GET['ym']) ? $_GET['ym']:($year.'-'.$month);
-$explode_ym = explode('-', $ym);//[0] => 2014 [1] => 5
+$explode_ym = explode('-', $ym);//[0] => 2014 [1] => 05
 $year_of_ym = $explode_ym[0];
 $month_of_ym = $explode_ym[1];
 
 //先月
 $last_month = array(
   'year' => date('Y', strtotime('last month', strtotime($year_of_ym.'-'.$month_of_ym.'-01'))),
-  'month' => date('n', strtotime('last month', strtotime($year_of_ym.'-'.$month_of_ym.'-01')))
+  'month' => date('m', strtotime('last month', strtotime($year_of_ym.'-'.$month_of_ym.'-01')))
 );
 //来月
 $next_month = array(
   'year' => date('Y', strtotime('next month', strtotime($year_of_ym.'-'.$month_of_ym.'-01'))),
-  'month' => date('n', strtotime('next month', strtotime($year_of_ym.'-'.$month_of_ym.'-01')))
+  'month' => date('m', strtotime('next month', strtotime($year_of_ym.'-'.$month_of_ym.'-01')))
 );
 
 
@@ -40,7 +46,7 @@ $after_cell  = array();
 if ($display_count % 2 == 1) {
     for ($i=1; $i<=$display_count; $i++) {
         $position = $i-(floor($display_count/2)+1);
-        $calendars[] = date("Y-n", mktime(0, 0, 0, $prev_month++, 1, $year_of_ym));
+        $calendars[] = date("Y-m", mktime(0, 0, 0, $prev_month++, 1, $year_of_ym));
         $before_cell[] = date('w', mktime(0, 0, 0, $prev_month2++, 1, $year_of_ym));
         $after_cell[]  = date('w', mktime(0, 0, 0, $prev_month3+1, 0, $year_of_ym));
         $prev_month3++;
@@ -50,8 +56,47 @@ if ($display_count % 2 == 1) {
 
 // Y-nを取得。$now_yeaeの前後1年
 for ($i=-12; $i<=12; $i++) {
-    $months[] = date('Y-n', mktime(0, 0, 0, $month+($i), 1, $year));
+    $months[] = date('Y-m', mktime(0, 0, 0, $month+($i), 1, $year));
 }
+// print_r($months);
+
+
+/*
+* Googlle Calendar API 祝日取得
+*/
+/*if ($_GET['ym'] == $year.'-'.$month) {
+    $ym = $year.'-'.$next_month['month'];
+}
+print_r($_GET['ym']);
+*/
+
+// print_r($ym);
+$holidays_url = sprintf(
+        "http://www.google.com/calendar/feeds/%s/public/full-noattendees?start-min=%s&start-max=%s&max-results=%d&alt=json" ,
+        "outid3el0qkcrsuf89fltf7a4qbacgt9@import.calendar.google.com" , // 'japanese@holiday.calendar.google.com' ,
+        "1970-01-01" ,  // 取得開始日
+        "2015-12-01" ,  // 取得終了日
+        10000             // 最大取得数
+        );
+if ( $results = file_get_contents($holidays_url) ) {
+        $results = json_decode($results, true);
+
+        $holidays = array();
+        foreach ($results['feed']['entry'] as $key =>$val ) {
+                $date  = $val['gd$when'][0]['startTime'];
+                $title = $val['title']['$t'];
+                $holidays[$date] = $title;// [2007-01-01] => 元日 / Ganjitsu / New Year's Day
+                // $holidays[$key]= array($date=>$title);
+                // print_r($val);
+
+        }
+        ksort($holidays);
+}
+
+
+// var_dump($holidays['2013-04-29']);
+// print_r($holidays);
+
 
 ?>
 
@@ -108,23 +153,29 @@ for ($i=-12; $i<=12; $i++) {
 
         <!-- 日付挿入 -->
         <?php for ($day=$start_day; $day<=$end_day[$key]; $day++):?>
+
+            <!-- 桁数を揃える -->
+            <?php $days = sprintf('%02d', $day) ; $hoge = $value."-".$days;?>
             <!-- 週末取得0~6 -->
             <?php $month_weekend=date("w", strtotime($value.'-'.$day));?>
-            <!-- 週末 -->
-            <?php if ($month_weekend == 0) :?>
-                <tr><td class="sunday"><?php echo $day ;?></td>
-            <?php endif ;?>
-            <?php if ($month_weekend == 6) :?>
-                <td class="saturday"><?php echo $day ;?></td></tr>
-            <?php endif ;?>
-            <!-- 週末以外 -->
-            <?php if ($month_weekend != 0 && $month_weekend != 6) :?>
-                <?php if ($today['mday'] === $day && $year.'-'.$month === $value) :?>
-                    <td class="today"><?php echo $day;?></td>
-                <?php else :?>
-                    <td><?php echo $day ;?></td>
+
+                <!-- 週末 -->
+                <?php if ($month_weekend == 0) :?>
+                    <td class="sunday"><?php echo $day ;?></td>
                 <?php endif ;?>
-            <?php endif ;?>
+
+                <?php if ($month_weekend == 6) :?>
+                    <td class="saturday"><?php echo $day ;?></td></tr>
+                <?php endif ;?>
+
+                <!-- 週末以外 -->
+                <?php if ($month_weekend != 0 && $month_weekend != 6) :?>
+                    <?php if (date('j') == $day && $year.'-'.$month === $value) :?>
+                        <td class="today"><?php echo $day;?></td>
+                    <?php else :?>
+                        <td><?php echo $day ;?></td>
+                    <?php endif ;?>
+                <?php endif ;?>
         <?php endfor ;?>
 
         <!-- 空セル挿入 -->
